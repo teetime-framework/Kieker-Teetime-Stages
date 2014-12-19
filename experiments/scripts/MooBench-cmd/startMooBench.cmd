46c8da3f57ad929992e6b886a43cebce3b0e7594
Christian Wulf <chw@informatik.uni-kiel.de>
2014-12-19 11:03:32 +0100

    added echo to startMooBench

--- experiments/scripts/MooBench-cmd/startMooBench.cmd
+++ experiments/scripts/MooBench-cmd/startMooBench.cmd
@@ -11,6 +11,8 @@ set cp=.;MooBench.jar;META-INF/kieker.monitoring.properties;META-INF/kieker.logg
 set jvmParams=-javaagent:lib/kieker-1.9_aspectj.jar -Dorg.aspectj.weaver.loadtime.configuration=META-INF/kieker.aop.xml -Dorg.aspectj.weaver.showWeaveInfo=true -Daj.weaving.verbose=true -Dkieker.monitoring.writer=kieker.monitoring.writer.tcp.TCPWriter
 set params=-d 10 -h 1 -m 0 -t %calls% -o tmp/test.txt -q
 
+echo params: %params%
+
 for /l %%i in (1, 1, %runs%) do (
 	java -cp %cp% %jvmParams% mooBench.benchmark.Benchmark %params%
 )
